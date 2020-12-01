d994d9945d3e5c0ebc2520ed6df26cbe45eb9618
MrGrim <19559207+GrimDevelopment@users.noreply.github.com>
2020-12-01 13:52:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const url 		= "https://blockchain.info/ticker";
+const url = "https://blockchain.info/ticker";
 
 function actualiserPrix() {
-	const req 	= new XMLHttpRequest();
-	const method 	= 'GET';
+	const req = new XMLHttpRequest();
+	const method = 'GET';
 	
 	req.open(method, url);
 	req.onreadystatechange = function(event) {
 		if (this.readyState === XMLHttpRequest.DONE) {
 			if (this.status === 200) {
 				const response = JSON.parse(this.responseText);
-				document.getElementById("price_label_dollars").textContent = response.EUR.buy;
 				document.getElementById("price_label_euros").textContent = response.USD.buy;
 			} else {
 				console.log("status :" + this.status);
-				alert("Quelque chose semble ne pas fonctionner, veuillez réessayer plus tard");
 			}
 		}
 	}
